d02a8e2f26e9b27a21e2ee596be8eff6a7d4ddcb
AChar1f <acharif41@gmail.com>
2024-09-02 09:32:22 +0200

    card comp added

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <slot name="cardHeader"></slot>
+        </div>
+        <div class="card-body">
+            <slot name="cardBody"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CardComp"
+}
+</script>
+
+<style scoped>
+.card {
+  width: 18rem;
+  padding-block: 9px;
+  background-image: linear-gradient(180deg, black 65%,#232323);
+  color: white;
+  border: 2px solid silver;
+}    
+</style>
