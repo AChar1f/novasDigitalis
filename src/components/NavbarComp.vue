7232af29736fec8ae9f2d06ad8eeda3e700fecfd
AChar1f <acharif41@gmail.com>
2024-09-02 11:58:09 +0200

    navbar comp added

---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,51 @@
+<template>
+<nav class="navbar navbar-expand-lg sticky-top">
+  <div class="container-fluid">
+    <router-link class="navbar-brand" to="/"><img src="https://achar1f.github.io/projectimages/Novas Digitalis/ndlogo.png" alt="logo"></router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <router-link class="nav-link"  to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/products">Products</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/contactUs">Contact Us</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/register">Register</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/login">Login</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/admin">Admin</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+
+<style scoped>
+.navbar{
+    background-color: #2C3E50;
+}
+
+.nav-link {
+    color: #D3D3D3;
+
+    &:hover{
+        color: #FFA500;
+        text-decoration: underline;
+    }
+}
+</style>
