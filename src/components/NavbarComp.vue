3f1ae3c908308588f3808ed88f351059deffced0
AChar1f <acharif41@gmail.com>
2024-09-03 12:42:09 +0200

    about content added

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="navbar navbar-expand-lg sticky-top">
   <div class="container-fluid">
-    <router-link class="navbar-brand" to="/"><img src="https://achar1f.github.io/projectimages/Novas Digitalis/ndlogo.png" alt="logo"></router-link>
+    <router-link class="navbar-brand" to="/"><img src="https://achar1f.github.io/projectimages/Novas Digitalis/LogoNoBg.png" alt="logo"></router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container-fluid about-cont">
+    <div class="container">
+      <h1>About Us</h1>
+      <ul>
+        <li>
+          Welcome to Novas Digitalis, your premier destination for cutting-edge
+          technology and expert guidance tailored specifically for first-time PC
+          buyers and builders. At Novas Digitalis, we understand that stepping
+          into the world of technology for the first time can be both exciting
+          and overwhelming. Our mission is to simplify the process by offering a
+          curated selection of high-quality tech products, accompanied by
+          comprehensive support to help you make informed decisions.
+        </li>
+        <li>
+          Our dedicated team is passionate about technology and committed to
+          ensuring that your first experience with PC purchasing and building is
+          smooth and successful. From top-tier components and peripherals to
+          personalized advice and resources, Novas Digitalis is here to empower
+          you every step of the way.
+        </li>
+        <li>
+          Whether you're assembling your dream PC or making your first tech
+          purchase, trust Novas Digitalis to provide the products and expertise
+          you need to achieve your goals with confidence. Discover a new level
+          of tech shopping with us today.
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.about-cont {
+  min-height: 85vh;
+  background: url(https://achar1f.github.io/projectimages/Novas%20Digitalis/About-BG.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  /* align-content: center; */
+}
+.container {
+  backdrop-filter: brightness(65%);
+}
+
+ul li {
+  list-style: none;
+  margin-block: 2rem;
+  
+}
+</style>
