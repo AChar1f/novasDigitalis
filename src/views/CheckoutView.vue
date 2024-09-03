f083c864946547597a01dc7d9983263416c0ecff
AChar1f <acharif41@gmail.com>
2024-09-03 09:51:45 +0200

    checkout template added

---
+++ src/views/CheckoutView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>This is the Checkout Page</h2>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "CheckoutView"
+    
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
